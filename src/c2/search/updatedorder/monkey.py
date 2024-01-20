4d2d55a656bb7dedeb9031dc43ab31569cbb54eb
Manabu TERADA <terada@cmscom.jp>
2024-01-20 16:01:19 +0900

    Remove okapi class

--- src/c2/search/updatedorder/monkey.py
+++ src/c2/search/updatedorder/monkey.py
@@ -4,9 +4,3 @@ from logging import getLogger
 
 logger = getLogger(__name__)
 info = logger.info
-
-
-add_index_types = {'Okapi BM25 Updated Rank': OkapiUpdatedIndex}
-
-ZCTextIndex.index_types.update(add_index_types)
-info("patched %s", str(ZCTextIndex.index_types))
